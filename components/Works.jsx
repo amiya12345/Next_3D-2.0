1699fa0f1a9d42d4e6a33b45bc60c82a9067408a
Amiya Ranjan sethi <amiyaranjansethi0551@gmail.com>
2023-07-22 09:47:01 +0530

    ar3d to moon

--- components/Works.jsx
+++ components/Works.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Ethp from "../public/assets/works/Ethp.webp";
+import Ethp from "../public/assets/works/eth/ethp.webp";
 import Type from "../public/assets/works/type.webp";
-import Shape from "../public/assets/works/Shape.webp";
-import Room from "../public/assets/works/Room.webp";
+import Shape from "../public/assets/works/shape/shape.webp";
+import Room from "../public/assets/works/room/room.webp";
 import Webi from "../public/assets/works/web/webi.webp";
-import Cybr from "../public/assets/works/cyber.webp";
+import Cybr from "../public/assets/works/cyber/cyber.webp";
 import WorksItem from "./Worksitem";
 
 const Projects = () => {
