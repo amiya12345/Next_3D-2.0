09ece147d2c55e6d17b8f63057b9afc41b80e04b
Amiya Ranjan sethi <amiyaranjansethi0551@gmail.com>
2023-07-22 14:00:43 +0530

    amioya

--- components/Main.jsx
+++ components/Main.jsx
@@ -42,7 +42,7 @@ const Main = () => {
         </p>
       </motion.div>
       <div className="flex-1">
-        <Spline scene="https://draft.spline.design/uX335H-fGqhRfyEv/scene.splinecode" />
+        <Spline scene="https://draft.spline.design/FCxHPakYEjrhJAcr/scene.splinecode" />
       </div>
     </section>
   );
