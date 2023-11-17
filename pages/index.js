6a1999eb0611f467ee6423bacff9fad3c9b25f30
amiya12345 <amiyaranjansethi0551@gmail.com>
2023-11-17 13:03:24 +0530

    Building

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ return (
 <title>AR3D-Portfolio of Amiya</title>
 <meta
 name    = "description"
-content = "Designed & Develope By Amiya using Next.js and Tailwind"
+content = "Designed & Developed By Amiya using Next.js and Tailwind"
 />
 <meta name = "viewport" content = "width=device-width, initial-scale=1.0"/>
 <link rel  = "icon" href        = "/favicon.ico" />
